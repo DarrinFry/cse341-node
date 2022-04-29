3af7b2a65b8bbf26d17bb73153b3dae23cb68a37
Darrin Fry <productionstagemanager@gmail.com>
2022-04-28 22:47:31 -0600

    week 2 attempt

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
-const routes = require('express').Router();
+// const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
-    res.send('Someone I know A.K.A. Darrin Fry (That was an attempt at some dad humor.)')
-});
+// routes.get('/', (req, res) => {
+//     res.send('Someone I know A.K.A. Darrin Fry (That was an attempt at some dad humor.)')
+// });
 
-module.exports = routes
+// module.exports = routes
+
+const express = require('express');
+const router = express.Router();
+
+router.use('/contacts', require('./contacts'))
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require("dotenv").config();
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-// const env = require("dotenv").config();
+// require("dotenv").config();
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 5000;
+// // const env = require("dotenv").config();
 
-app.use('/', require('./routes'))
+// app.use('/', require('./routes'))
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Running on port ${port}`)
+// })
  
 // const MongoClient = require('mongodb').MongoClient;
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cse341.jubgr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -21,3 +21,27 @@ app.listen(port, () => {
 //         db.close()
 //     });
 // });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
