e2611ddf3f5f2b4ecd69540c37cbc915f072c0ec
Darrin Fry <productionstagemanager@gmail.com>
2022-04-28 19:37:43 -0600

    Revert "spelling error"
    This reverts commit f21592a1787137c2251c4ac7329cb1cf050ef6ed.

--- week1/routes/index.js
+++ week1/routes/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
-    res.send('Someone I know A.K.A. Darrin Fry (That was an attempt at some dad humor.)')
+    res.send('Someone I know A.K.K Darrin Fry (That was an attempt at some dad humor.)')
 });
 
 module.exports = routes
