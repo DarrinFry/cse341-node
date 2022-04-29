1c875e30a9e64ed04034694d7ef36001f9c8ed24
Darrin Fry <productionstagemanager@gmail.com>
2022-04-28 23:17:37 -0600

    Adjustement to const variables and env file

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db('test').collection('user').find();
+  const result = await mongodb.getDb().db(process.env.PARENT_FOLDER).collection(process.env.CHILD_FOLDER).find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -13,8 +13,8 @@ const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db('test')
-    .collection('user')
+    .db(process.env.PARENT_FOLDER)
+    .collection(process.env.CHILD_FOLDER)
     .find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
-// const routes = require('express').Router();
+const routes = require('express').Router();
 
 // routes.get('/', (req, res) => {
 //     res.send('Someone I know A.K.A. Darrin Fry (That was an attempt at some dad humor.)')
 // });
 
-// module.exports = routes
+routes.use('/contacts', require('./contacts'))
 
-const express = require('express');
-const router = express.Router();
-
-router.use('/contacts', require('./contacts'))
-
-module.exports = router;
+module.exports = routes;
